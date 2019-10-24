35feb5756ef1c4b5f8d092027b102cd0ad4cbd7a
leo <lyon12189@gmail.com>
2019-10-23 20:42:53 -0500

    yield

--- routes/web.php
+++ routes/web.php
@@ -8,9 +8,6 @@
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -18,8 +15,12 @@ Route::get('/', function () {
 Route::get('foo', function () {
     return 'Hola esto es una prueba';
 });
+|
+*/
 
-Route::get('permiso', 'PermisoController@index');
+
+Route::get('/', 'InicioController@index');
+//Route::get('permiso', 'PermisoController@index');
 //Route::view('permiso', 'permiso');
 
-Route::get('admin/sistema/permiso', 'PermisoController@index')->name('permiso');
+//Route::get('admin/sistema/permiso', 'PermisoController@index')->name('permiso');
